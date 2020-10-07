e7602bd3cd979af45d2ef8b1482ff6d28d10f60b
sloth-hub <madamhh@gmail.com>
2020-10-07 21:25:30 +0900

    Fix css and weather images

--- src/components/Food.js
+++ src/components/Food.js
@@ -21,14 +21,16 @@ const Food = ({ state, toggle, setToggle }) => {
     }
     return (
         <div className="food_container">
-            <h1 className="food_ask">지금 어떤 맛이 땡기나요?</h1>
-            <div className="foods">
-                <input type="radio" name="taste" value="spicy" id="spicy" onChange={handleFoodChange} /><label htmlFor="spicy">매운거</label>
-                <input type="radio" name="taste" value="oily" id="oily" onChange={handleFoodChange} /><label htmlFor="oily">기름진거</label>
-                <input type="radio" name="taste" value="light" id="light" onChange={handleFoodChange} /><label htmlFor="light">담백한거</label>
-                <input type="radio" name="taste" value="soup" id="soup" onChange={handleFoodChange} /><label htmlFor="soup">호로록 국물있는거</label>
+            <div className="food_box">
+                <h1 className="food_ask">지금 어떤 맛이 땡기나요?</h1>
+                <div className="foods">
+                    <input type="radio" name="taste" value="spicy" id="spicy" onChange={handleFoodChange} /><label htmlFor="spicy">매운거</label>
+                    <input type="radio" name="taste" value="oily" id="oily" onChange={handleFoodChange} /><label htmlFor="oily">기름진거</label>
+                    <input type="radio" name="taste" value="light" id="light" onChange={handleFoodChange} /><label htmlFor="light">담백한거</label>
+                    <input type="radio" name="taste" value="soup" id="soup" onChange={handleFoodChange} /><label htmlFor="soup">호로록 국물있는거</label>
+                </div>
+                <button className="food_next btn" onClick={handleFoodClick}>NEXT</button>
             </div>
-            <button className="food_next btn" onClick={handleFoodClick}>NEXT</button>
         </div>
     );
 }
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -19,12 +19,8 @@ const weatherOptions = {
         title: "Rain"
     },
     Snow: {
-        iconName: "/images/003-snowy.png",
+        iconName: "/images/025-snow.png",
         title: "Snow"
-    },
-    Atmosphere: {
-        iconName: "/images/009-cloudy.png",
-        title: "Atmosphere"
     },
     Clear: {
         iconName: "/images/005-sun.png",
@@ -42,15 +38,18 @@ const weatherOptions = {
         iconName: "/images/012-windy.png",
         title: "Dust"
     },
+    Tornado: {
+        iconName: "/images/015-tornado.png",
+        title: "Tornado"
+    }
 }
 const Weather = ({ temp, condition }) => {
 
     return (
-        <ul>
-            <li className="weatherTemp">{Math.round(temp)}˚C</li>
-            <li><img src={weatherOptions[condition].iconName} className="weatherIcon" alt={condition} /></li>
-            <li className="weatherTitle">{weatherOptions[condition].title}</li>
-        </ul>
+        <div className="weatherbox">
+            <p className="weatherTemp">{Math.round(temp)}˚C</p>
+            <img src={weatherOptions[condition].iconName} className="weatherIcon" alt={condition} />
+        </div>
     );
 }
 
