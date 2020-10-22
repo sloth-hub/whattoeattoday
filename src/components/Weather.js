130d7180020aaed617ec4f13443de65164bc1e36
sloth-hub <madamhh@gmail.com>
2020-10-22 16:48:33 +0900

    Rename img to images

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,43 +3,43 @@ import PropTypes from "prop-types";
 
 const weatherOptions = {
     Haze: {
-        iconName: "/img/009-cloudy.png",
+        iconName: "/images/009-cloudy.png",
         title: "Haze"
     },
     Thunderstorm: {
-        iconName: "/img/013-thunder.png",
+        iconName: "/images/013-thunder.png",
         title: "Thunderstrom"
     },
     Drizzle: {
-        iconName: "/img/021-rain.png",
+        iconName: "/images/021-rain.png",
         title: "Drizzle"
     },
     Rain: {
-        iconName: "/img/002-rainy.png",
+        iconName: "/images/002-rainy.png",
         title: "Rain"
     },
     Snow: {
-        iconName: "/img/025-snow.png",
+        iconName: "/images/025-snow.png",
         title: "Snow"
     },
     Clear: {
-        iconName: "/img/005-sun.png",
+        iconName: "/images/005-sun.png",
         title: "Clear"
     },
     Clouds: {
-        iconName: "/img/001-cloud.png",
+        iconName: "/images/001-cloud.png",
         title: "Clouds"
     },
     Mist: {
-        iconName: "/img/016-mist.png",
+        iconName: "/images/016-mist.png",
         title: "Mist"
     },
     Dust: {
-        iconName: "/img/012-windy.png",
+        iconName: "/images/012-windy.png",
         title: "Dust"
     },
     Tornado: {
-        iconName: "/img/015-tornado.png",
+        iconName: "/images/015-tornado.png",
         title: "Tornado"
     }
 }
