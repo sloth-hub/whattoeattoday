90962d8464af388927f4e6825640ed7134c7566a
sloth-hub <madamhh@gmail.com>
2020-10-22 13:25:19 +0900

    Updates

---
+++ precache-manifest.5186af8516d8111318769a7284b01626.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8406052bb22d73317f00775ee9a4c22a",
+    "url": "/what_to_eat_today/index.html"
+  },
+  {
+    "revision": "11025e87589c7985baed",
+    "url": "/what_to_eat_today/static/css/main.259176a3.chunk.css"
+  },
+  {
+    "revision": "fff94e20b676555438c0",
+    "url": "/what_to_eat_today/static/js/2.75ce8006.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/what_to_eat_today/static/js/2.75ce8006.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11025e87589c7985baed",
+    "url": "/what_to_eat_today/static/js/main.baa8aacb.chunk.js"
+  },
+  {
+    "revision": "a3572e4804a2960a8075",
+    "url": "/what_to_eat_today/static/js/runtime-main.81ea0c6b.js"
+  }
+]);
