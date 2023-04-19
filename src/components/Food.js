ba68bb1ecff6dc5ba8e9e68984d7061fac89d553
sloth-hub <madamhh@gmail.com>
2023-04-19 22:22:47 +0900

    Chore: 이미지 파일명 변경

--- src/components/Food.js
+++ src/components/Food.js
@@ -41,22 +41,22 @@ const Food = ({ mood }) => {
                     <div className="foods">
                         <label htmlFor="spicy">
                             <input type="radio" name="taste" className="spicy" id="spicy" value="spicy" onChange={handleFoodChange} />
-                            <img src={process.env.PUBLIC_URL + "/images/spicy_select.png"} alt="spicy" />
+                            <img src={process.env.PUBLIC_URL + "/images/spicy.png"} alt="spicy" />
                             <span className="food_text">매운맛</span>
                         </label>
                         <label htmlFor="oily">
                             <input type="radio" name="taste" className="oily" id="oily" value="oily" onChange={handleFoodChange} />
-                            <img src={process.env.PUBLIC_URL + "/images/oily_select.png"} alt="oily" />
+                            <img src={process.env.PUBLIC_URL + "/images/oily.png"} alt="oily" />
                             <span className="food_text">기름진맛</span>
                         </label>
                         <label htmlFor="light">
                             <input type="radio" name="taste" className="light" id="light" value="light" onChange={handleFoodChange} />
-                            <img src={process.env.PUBLIC_URL + "/images/light_select.png"} alt="light" />
+                            <img src={process.env.PUBLIC_URL + "/images/light.png"} alt="light" />
                             <span className="food_text">담백한맛</span>
                         </label>
                         <label htmlFor="soup">
                             <input type="radio" name="taste" className="soup" id="soup" value="soup" onChange={handleFoodChange} />
-                            <img src={process.env.PUBLIC_URL + "/images/soup_select.png"} alt="soup" />
+                            <img src={process.env.PUBLIC_URL + "/images/soup.png"} alt="soup" />
                             <span className="food_text">국탕면</span>
                         </label>
                     </div>
--- src/components/Mood.js
+++ src/components/Mood.js
@@ -30,23 +30,23 @@ const Mood = () => {
                     <div className="moods">
                         <label htmlFor="good">
                             <input type="radio" name="mood" className="good" id="good" value="good" onChange={handleMoodChange} />
-                            <img src={process.env.PUBLIC_URL + "/images/001-icon-569501_select.png"} alt="good" />
+                            <img src={process.env.PUBLIC_URL + "/images/001-icon-569501.png"} alt="good" />
                         </label>
                         <label htmlFor="happy">
                             <input type="radio" name="mood" className="happy" id="happy" value="happy" onChange={handleMoodChange} />
-                            <img src={process.env.PUBLIC_URL + "/images/002-icon-569531_select.png"} alt="happy" />
+                            <img src={process.env.PUBLIC_URL + "/images/002-icon-569531.png"} alt="happy" />
                         </label>
                         <label htmlFor="melancholy">
                             <input type="radio" name="mood" className="melancholy" id="melancholy" value="melancholy" onChange={handleMoodChange} />
-                            <img src={process.env.PUBLIC_URL + "/images/003-icon-569513_select.png"} alt="melancholy" />
+                            <img src={process.env.PUBLIC_URL + "/images/003-icon-569513.png"} alt="melancholy" />
                         </label>
                         <label htmlFor="sad">
                             <input type="radio" name="mood" className="sad" id="sad" value="sad" onChange={handleMoodChange} />
-                            <img src={process.env.PUBLIC_URL + "/images/004-icon-569569_select.png"} alt="sad" />
+                            <img src={process.env.PUBLIC_URL + "/images/004-icon-569569.png"} alt="sad" />
                         </label>
                         <label htmlFor="angry">
                             <input type="radio" name="mood" className="angry" id="angry" value="angry" onChange={handleMoodChange} />
-                            <img src={process.env.PUBLIC_URL + "/images/005-icon-569518_select.png"} alt="angry" />
+                            <img src={process.env.PUBLIC_URL + "/images/005-icon-569518.png"} alt="angry" />
                         </label>
                     </div>
                     <button className="mood_next btn" onClick={handleMoodClick}>NEXT</button>
